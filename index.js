5b113e15f887a7cd366b45027dc02eb52f87bb43
Shunyavkash Admin <132559941+shunyavkash-admin@users.noreply.github.com>
2025-09-22 18:15:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.get("/", (req, res) => {
   res.json(info);
 });
 
-const server = app.listen(3001, () => {
+const server = app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
